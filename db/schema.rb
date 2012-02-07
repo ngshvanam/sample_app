2de270c6c93cf5f740c0e20b51a284a785872e3e
Nagesh <nag.501@gmail.com>
2012-02-07 16:45:56 +0530

    Modelling User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120207092544) do
+ActiveRecord::Schema.define(:version => 20120207101547) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end

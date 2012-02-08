2d721c003a8abc5e594007b6acee6b613d3b1c51
Nagesh <nag.501@gmail.com>
2012-02-08 17:54:02 +0530

    Current User Error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index,:edit, :update]
-  before_filter :current_user, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
 
   def show
     @user = User.find(params[:id])
   end
+
   def new
-  @user = User.new
-  @title = "Sign up"
+    @user = User.new
+    @title = "Sign up"
   end
-   def create
+  
+  def create
     @user = User.new(params[:user])
     if @user.save
 	 sign_in @user
@@ -21,10 +23,12 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   def edit
-   @user = User.find(params[:id])
-   @title = "Edit user"
+    @user = User.find(params[:id])
+    @title = "Edit user"
   end
+  
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
@@ -46,6 +50,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+  
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
@@ -56,11 +61,12 @@ class UsersController < ApplicationController
     def authenticate
       deny_access unless signed_in?
     end
-	 def correct_user
+	def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-	 def admin_user
+	
+	def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,9 @@ end
 def current_user
     @current_user||= user_from_remember_token
 end
+def current_user?(user)
+  @current_user == user
+end
  def signed_in?
     !current_user.nil?
   end

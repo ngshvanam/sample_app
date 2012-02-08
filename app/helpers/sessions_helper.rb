4029f6588805d4cf436d6ea236a6e3ceae6d8ca6
Nagesh <nag.501@gmail.com>
2012-02-08 15:09:16 +0530

    Updating User:

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,6 +19,16 @@ end
     self.current_user = nil
   end
 
+  def deny_access
+   store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
  private
 
     def user_from_remember_token
@@ -27,5 +37,12 @@ end
 
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+	 def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end

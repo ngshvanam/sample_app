f545ce894f8f96d7f14f0488359e7024bcc620d9
Nagesh <nag.501@gmail.com>
2012-02-08 16:59:53 +0530

    Delete Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index,:edit, :update]
   before_filter :current_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
   def show
     @user = User.find(params[:id])
@@ -45,6 +46,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
 
     private
     def authenticate
@@ -53,5 +59,8 @@ class UsersController < ApplicationController
 	 def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+	 def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end

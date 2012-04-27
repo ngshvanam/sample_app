0b21f2bf589c95656d5fe8a921e45abdf769ac2f
Nagesh <nag.501@gmail.com>
2012-04-27 22:31:36 +0530

    sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ def home
 	 # @response = params[:signed_request];
 	   @response = ActiveSupport::JSON.decode(Base64.decode64(params[:signed_request].split(".").last))["oauth_token"]
   else
-   redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes,friends_photos"
+   redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes,friends_photos,read_friendlists"
 	@response = ""
   end
 

ed090e35e0ce4de97692eedf8323aa0603976d62
Nagesh <nag.501@gmail.com>
2012-04-27 20:37:51 +0530

    asd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
 def home
+redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
+  @response = params[:signed_request];
 end
 
 end

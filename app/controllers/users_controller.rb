878dfda2f76422e2e5740d8ad299bc11228a2018
Nagesh <nag.501@gmail.com>
2012-04-27 23:09:41 +0530

    sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ require "base64"
 def home
   if request.post?
 	 # @response = params[:signed_request]
-	   @response = ActiveSupport::JSON.decode(Base64.decode64(params[:signed_request].split(".").last))["oauth_token"]
+	   @response = Base64.decode64(params[:signed_request].split(".").last)
   else
     redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
 	@response = ""

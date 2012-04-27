cf0c996f66d68b005e9bc5f58b442efc577b1f8b
Nagesh <nag.501@gmail.com>
2012-04-27 21:41:04 +0530

    asd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
+require "base64"
 def home
-if request.post?
-	  @response = params[:signed_request];
-else
-redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
-	   @response = "";	
-end
+  if request.post?
+	 # @response = params[:signed_request];
+	   @response = Base64.decode64(params[:signed_request].split(".").last)
+  else
+   redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
+   @response = "";	
+  end
 
 end
 

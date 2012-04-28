47fce55825069cfbe4e344afd92a2a2e67c98dbf
Nagesh <nag.501@gmail.com>
2012-04-28 20:22:03 +0530

    sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ require "base64"
 
 def home
   if request.post?
+	redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
 	   @response = Base64.decode64(params[:signed_request].split(".").last)
   else
     redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"

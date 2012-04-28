b60b960b9d0c85e63ac94439af5f65b4b0d234d2
Nagesh <nag.501@gmail.com>
2012-04-28 20:45:46 +0530

    Sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,13 @@ require "base64"
 
 def home
   if request.post?
-	#redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
-	   @response = Base64.decode64(params[:signed_request].split(".").last)
+	@response = ActiveSupport::JSON.decode(Base64.decode64(params[:signed_request].split(".").last))["oauth_token"]
+	if @response.nil?
+	 redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
+	end
   else
-    redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
+    # redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
+	# @response = ActiveSupport::JSON.decode('{"algorithm":"HMAC-SHA256","issued_at":1335625281,"user":{"country":"in","locale":"en_US","age":{"min":21}}}')["oauth_token"].nil?
   end
 
 end

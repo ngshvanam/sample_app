c56d06bf98c5c6f30d9f4f58729f724d5dfac31f
Nagesh <nag.501@gmail.com>
2012-05-21 22:32:15 +0530

    sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,7 @@ require 'open-uri'
 def home
   if request.post?
 	$jsondata = ActiveSupport::JSON.decode(Base64.decode64(params[:signed_request].split(".").last))
-	begin
         $response = $jsondata["oauth_token"]
-	rescue
-	 puts "sdf"
-	end
   else
     # redirect_to "https://www.facebook.com/dialog/oauth/?client_id=268196199939502&redirect_uri=https%3A%2F%2Fapps.facebook.com%2Fnag_app%2F&scope=user_location,user_likes,friends_likes"
 	
